d21cd40e7ceb4bd28b49ac26ffeff6ce37b492f4
Andrew <andrewcboardman6@gmail.com>
2023-03-14 12:48:12 +0000

    Make into package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pyGEMME',
+    version='0.1.0',
+    install_requires=['certifi==2022.12.7'],
+    packages=find_packages(),
+    package_dir={},
+    # url='https://github.com/your-name/your_app',
+    # license='MIT',
+    # author='Your NAME',
+    # author_email='your@email.com',
+    # description='Your main project',
+)
